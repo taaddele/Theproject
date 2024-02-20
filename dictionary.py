a8d0f45fe0b1296af009ed0fc30fac7601480e7d
Taddele petros <82232055+taaddele@users.noreply.github.com>
2024-02-20 11:18:21 +0300

    Delete dictionary.py

--- dictionary.py
+++
@@ -1,9 +0,0 @@
-#defining dictionaries
-disease = {                # one method of defining
-    'malaria':'mosquito',
-    'hiv':'virus',
-    'athletes foot':'fungi'
-}
-books = dict(programming='java',database='sql')
-print(disease['hiv'])
-print(disease)
