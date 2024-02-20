9a8fe968783209ee00bd368770ffdb7ae0111776
Taddele petros <82232055+taaddele@users.noreply.github.com>
2024-02-20 11:21:06 +0300

    Delete tuples.py

--- tuples.py
+++
@@ -1,8 +0,0 @@
-# defining tuples
-# tuples are ordered and unchangable(immutable)
-# tuples are declared by round brackets 
-books=tuple(('java','c++','networking','database'))
-names=('almaz','dubale','ayenewu')
-#books[0]='c#'--> tuple object doesn't support item assignment
-print(books)
-print(names)
